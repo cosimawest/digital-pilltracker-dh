fc4bff86383c6d6a00ccc1df830dfdd52e648f4e
catherinehealyy <catherinehealy@ufl.edu>
2022-04-09 16:13:13 -0400

    add medicine button working, listing the meds working

--- App.js
+++ App.js
@@ -1,46 +1,82 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
-
-import Screen from './Screen.js';
-
-const data = [
-  {id: 'a', value: 'A'},
-  {id: 'b', value: 'B'},
-  {id: 'c', value: 'C'},
-  {id: 'd', value: 'D'},
-];
-
-const numColumns = 2;
-const size = Dimensions.get('window').width/numColumns;
-const height = Dimensions.get('window').height/Math.ceil(data.length/numColumns);
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import { IconButton, Colors } from "react-native-paper";
+import Screen from "./Screen.js";
 
 export default function App() {
+  const [Data, setData] = useState([{ id: "a", value: "A" }]);
+  const numColumns = 2;
+  const [Height, setHeight] = useState(
+    (Dimensions.get("window").height - 150) /
+      Math.ceil(Data.length / numColumns)
+  );
 
-  return (
-    
-    <View style={styles.home}>
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <View style={styles.screen}>
-            <Screen/>
-          </View>
-        )}
-        numColumns={numColumns} />
+  useEffect(() => {
+    setData(Data);
+  }, []);
+
+  const addMedicine = () => {
+    var tempArr = Data;
+    if (tempArr.length == 4) {
+      alert("Max number of medications is 4");
+    } else if (tempArr.length == 1) {
+      tempArr.push({ id: "b", value: "B" });
+      setHeight(
+        (Dimensions.get("window").height - 150) /
+          Math.ceil(Data.length / numColumns) /
+          2
+      );
+    } else if (tempArr.length == 2) {
+      tempArr.push({ id: "c", value: "C" });
+    } else if (tempArr.length == 3) {
+      tempArr.push({ id: "d", value: "D" });
+    }
+    setData(tempArr);
+
+    // alert("number of meds" + Data.length + "    height size" + Height);
+  };
+
+  const Render_Item = () => (
+    <View style={{ flex: 1, height: Height }}>
+      <Screen />
     </View>
   );
-}
 
+  return (
+    <SafeAreaView style={styles.home}>
+      <IconButton
+        style={styles.addPillButton}
+        icon="plus"
+        onPress={addMedicine}
+      ></IconButton>
+      <FlatList data={Data} renderItem={({ item }) => <Render_Item />} />
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   home: {
     flex: 1,
     backgroundColor: "#56CCF2",
   },
+  test: {
+    width: "100%",
+    height: "50%",
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
   screen: {
     flex: 1,
-    height: height,
-  }
+  },
+  addPillButton: {
+    alignSelf: "flex-end",
+  },
 });
-
-
--- Screen.js
+++ Screen.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, Alert } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
-import DateTimePicker from '@react-native-community/datetimepicker';
-
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Screen() {
   const [medName, setMedName] = useState("");
@@ -10,62 +17,68 @@ export default function Screen() {
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState("edit");
 
-
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
     setDate(currentDate);
   };
 
   const submit = () => {
-    if(medName === "") alert('Please enter a name for your medication');
-    else setMode("error")
-  }
-
-
+    if (medName === "") alert("Please enter a name for your medication");
+    else setMode("error");
+  };
 
   return (
     <View style={styles.home}>
-      {mode === "edit" ?
+      {mode === "edit" ? (
         <View style={styles.editView}>
           <TextInput
-              style={styles.title}
-              editable
-              maxLength={20}
-              value={medName}
-              onChangeText={input => setMedName(input)}
-              placeholder='Medication Name'
-            />
+            style={styles.title}
+            editable
+            maxLength={20}
+            value={medName}
+            onChangeText={(input) => setMedName(input)}
+            placeholder="Medication Name"
+          />
           <DateTimePicker
             value={date}
-            mode='time'
+            mode="time"
             onChange={onChange}
-            display='spinner'
+            display="spinner"
           />
-          <Button 
-            style={styles.done}
-            title='Done'
-            onPress={submit}>
-          </Button>
-        </View> 
-      : null }
-      {mode === "success" ?
+          <Button style={styles.done} title="Done" onPress={submit}></Button>
+        </View>
+      ) : null}
+      {mode === "success" ? (
         <View style={styles.sucessView}>
-          <Text style={styles.title}>You took your {date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</Text>
+          <Text style={styles.title}>
+            You took your{" "}
+            {date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </Text>
           <Text style={styles.title}>{medName}</Text>
           <Text style={styles.titleTwo}>Good job!</Text>
-        </View> 
-      : null }
-      {mode === "error" ?
+        </View>
+      ) : null}
+      {mode === "error" ? (
         <View style={styles.errorView}>
-            <Text style={styles.title}>You haven't taken your {date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</Text>
-            <Text style={styles.title}>{medName}</Text>
-            <TouchableOpacity
-                style={styles.takenButton}
-                onPress={() => setMode("success")}>
-                    <Text style={styles.takenButtonText}>I took it</Text>
-            </TouchableOpacity>
-        </View> 
-      : null }
+          <Text style={styles.title}>
+            You haven't taken your{" "}
+            {date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </Text>
+          <Text style={styles.title}>{medName}</Text>
+          <TouchableOpacity
+            style={styles.takenButton}
+            onPress={() => setMode("success")}
+          >
+            <Text style={styles.takenButtonText}>I took it</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
@@ -77,47 +90,45 @@ const styles = StyleSheet.create({
   editView: {
     backgroundColor: "#56CCF2",
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   sucessView: {
     backgroundColor: "#6FCF97",
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   errorView: {
     backgroundColor: "#EB5757",
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   titleTwo: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 17,
     paddingTop: 40,
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 17,
   },
   takenButton: {
     alignItems: "center",
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingTop: 40,
   },
   takenButtonText: {
-    backgroundColor: '#D91E1E',
+    backgroundColor: "#D91E1E",
     paddingVertical: 3,
     paddingHorizontal: 13,
     borderRadius: 5,
-    color: '#940000',
+    color: "#940000",
     fontSize: 17,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   subtitle: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   displayTime: {
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
-
-
