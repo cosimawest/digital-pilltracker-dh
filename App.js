9c09a30cbf91711d504bb07cb4740c663b1a9a01
cosimawest <cosimawest@ufl.edu>
2022-04-17 01:43:20 -0400

    notifs and remove screens funct

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  Dimensions,
-} from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Dimensions } from 'react-native';
 import { IconButton } from "react-native-paper";
 
 import Screen from './Screen.js';
@@ -13,18 +8,17 @@ import Screen from './Screen.js';
 const numColumns = 2;
 
 export default function App() {
-  const [data, setData] = useState([{id: 'a', value: 'A'}]);
+  const [data, setData] = useState([{id: 0, value: 'edit'}]);
   const [height, setHeight] = useState(Dimensions.get('window').height/Math.ceil(data.length/numColumns));
 
 
   const addMedicine = () => {
     if(data.length === 4) alert("Max number of medications is 4");
     else {
-    setData(data => [...data, {id: 'a', value: 'A'}]);
+    setData(item => [...item, {id: data.length, value: 'edit'}]);
     setHeight((Dimensions.get("window").height) /Math.ceil((data.length + 1) / numColumns));
     }
   };
-
 
   return (
     <View style={styles.home}>
@@ -38,14 +32,18 @@ export default function App() {
         data={data}
         renderItem={({item}) => (
           <View style={{flex: 1, height: height}}>
-            <Screen/>
+            <Screen 
+              id={data.id}
+              item={item}
+              data={data}
+              setData={setData}
+              setHeight={setHeight}/>
           </View>
         )}
         numColumns={numColumns} />
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   addPillButton: {
@@ -62,5 +60,3 @@ const styles = StyleSheet.create({
     flex: 1,
   }
 });
-
-
--- Screen.js
+++ Screen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   View,
   Text,
@@ -7,28 +7,158 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
+  Dimensions,
 } from "react-native";
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import storage from "@react-native-async-storage/async-storage";
+import { IconButton } from "react-native-paper";
 
 import DateTimePicker from "@react-native-community/datetimepicker";
 
-export default function Screen() {
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true
+  })
+});
+  
+const Screen = (props) => {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
   const [medName, setMedName] = useState("");
 
   const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState("edit");
+  const [mode, setMode] = useState(props.item.value);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
     setDate(currentDate);
   };
 
-  const submit = () => {
+  const submit = async () => {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: "Time to take " + medName,
+            body: 'Have you taken it yet?',
+            categoryIdentifier: medName,
+            badge: 1,
+        },
+        trigger: { 
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            repeats: true
+        },
+    });
+
     if (medName === "") alert("Please enter a name for your medication");
-    else setMode("error");
+    else {
+      setMode("error");
+      props.item.value = "error";
+    }
   };
+
+  useEffect(() => {
+    const getPermission = async () => {
+      if (Constants.isDevice) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            alert('Enable push notifications to use the app!');
+            await storage.setItem('expopushtoken', "");
+            return;
+          }
+          const token = (await Notifications.getExpoPushTokenAsync()).data;
+          await storage.setItem('expopushtoken', token);
+      } else {
+        alert('Must use physical device for Push Notifications');
+      }
+
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+    }
+
+    getPermission();
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {});
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };    
+  }, []);
+
+  useEffect(() => {
+    Notifications.setNotificationCategoryAsync(medName, [
+        { identifier: medName, 
+          buttonTitle: "Yep!",
+          options: {
+            opensAppToForeground: false,
+          },
+        },
+        { identifier: "No", 
+          buttonTitle: "Not yet",
+          options: {
+            opensAppToForeground: false,
+          },
+        },
+      ]).then(result => {
+
+      })
+
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+        if(response.actionIdentifier === medName) setMode("success");
+    });
+
+    return () => {
+        subscription.remove();
+      }; 
+  }, [medName]);
+
+  const removeMedicine = () => {
+    Alert.alert(
+      'Hold on!',
+      "Are you sure you want to delete " + medName + "?",
+      [{
+        text: 'Yes',
+        onPress: () => {
+          props.setHeight((Dimensions.get("window").height) /Math.ceil((props.data.length - 1) / 2));
+          props.setData(props.data.filter(item => item != props.item))
+        }
+      },{ text: 'No' }]
+    )
+  };
+
 
   return (
     <View style={styles.home}>
+      {props.data.length != 1 ?
+        <IconButton
+          style={styles.minusPillButton}
+          icon="minus"
+          onPress={removeMedicine}
+        /> : null
+      }
+      
+
       {mode === "edit" ? (
         <View style={styles.editView}>
           <TextInput
@@ -87,6 +217,12 @@ const styles = StyleSheet.create({
   home: {
     flex: 1,
   },
+  minusPillButton: {
+    zIndex: 2,
+    position: 'absolute',
+    bottom: 8,
+    left: 0,
+  },
   editView: {
     backgroundColor: "#56CCF2",
     flex: 1,
@@ -132,3 +268,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+export default Screen;
